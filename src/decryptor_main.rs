8382ea2719c57df9064facfb38a0b57bd50d6fb9
sudo-G41 <sudo.g41@gmail.com>
2024-08-23 06:18:51 +0000

    복호화

--- src/decryptor_main.rs
+++ src/decryptor_main.rs
@@ -1,23 +1,42 @@
 mod decryptor;
+mod readme;
 
+use readme::DelReadme;
 use mysql::*;
 use mysql::prelude::*;
 use std::io::{self, Write};
 
 fn main() -> Result<(), mysql::Error> {
-    // MySQL 접속 정보
-    let user = "root";
-    let url = "127.0.0.1";
-    let port = 13306;
+	// 사용자로부터 MySQL 접속 정보를 입력받음
+    print!("Enter MySQL username: ");
+    io::stdout().flush()?; // 출력 버퍼를 비워서 메시지를 즉시 출력
+    let mut user = String::new();
+    io::stdin().read_line(&mut user)?; // 사용자 이름 입력 받기
+    let user = user.trim();
 
-    // 사용자로부터 MySQL root 비밀번호를 입력받음
+    print!("Enter MySQL server URL: ");
+    io::stdout().flush()?;
+    let mut url = String::new();
+    io::stdin().read_line(&mut url)?; // 서버 URL 입력 받기
+    let url = url.trim();
+
+    print!("Enter MySQL server port: ");
+    io::stdout().flush()?;
+    let mut port_str = String::new();
+    io::stdin().read_line(&mut port_str)?; // 포트 입력 받기
+    let port: u16 = port_str.trim().parse().expect("Port must be a number");
+
     print!("Enter MySQL root password: ");
     io::stdout().flush()?;
     let mut passwd = String::new();
-    io::stdin().read_line(&mut passwd)?;
+    io::stdin().read_line(&mut passwd)?; // 비밀번호 입력 받기
     let passwd = passwd.trim();
 
-    let database = "example_db";
+    print!("Enter MySQL database name: ");
+    io::stdout().flush()?;
+    let mut database = String::new();
+    io::stdin().read_line(&mut database)?; // 데이터베이스 이름 입력 받기
+    let database = database.trim();
 
     // MySQL 옵션 설정
     let opts = OptsBuilder::new()
@@ -30,6 +49,11 @@ fn main() -> Result<(), mysql::Error> {
     // MySQL 서버에 연결
     let pool = Pool::new(opts)?;
     let mut conn = pool.get_conn()?;
+
+	let mut db_utils = DelReadme::new(user, passwd, url, port, database)?;
+
+    // README 테이블 삭제
+    db_utils.drop_table("README")?;
 
     // 복호화 모듈 초기화
     let key = b"an example very very secret key."; // 32 bytes
